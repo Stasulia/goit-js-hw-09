133ed07efaa373bdb6f45a145588b5999907ac0f
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-10-13 20:01:26 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const elements = {
+body: document.querySelector('body'),
+startBtn: document.querySelector('button[data-start]'),
+stopBtn: document.querySelector('button[data-stop]'),
+};
 
+elements.startBtn.addEventListener('click', handlerStart);
+elements.stopBtn.addEventListener('click', handlerStop);
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  function handlerStart() {
+    elements.startBtn.disabled = true;
+    elements.stopBtn.disabled = false;
+    idColor = setInterval(() => {
+    const color = getRandomHexColor();
+    elements.body.style.backgroundColor = color;
+    }, 1000);
+}
+
+function handlerStop() {
+    elements.startBtn.disabled = false;
+    elements.stopBtn.disabled = true;
+   // elements.body.style.backgroundColor = 'transparent';
+   clearInterval(idColor);
+}
