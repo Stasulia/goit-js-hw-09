fa576ebc062352ae4402b666f771537abc587f66
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-10-18 18:26:44 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,15 +8,13 @@ refs.startBtn.addEventListener('click', handlerStart);
 refs.stopBtn.addEventListener('click', handlerStop);
 
 let idColor = null;
-isDisabled = true;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
 
   function handlerStart() {
-    refs.startBtn = isDisabled;
-    refs.stopBtn = !isDisabled;
+    toggleBtnState (true, false);
     idColor = setInterval(() => {
     const color = getRandomHexColor();
     refs.body.style.backgroundColor = color;
@@ -24,7 +22,11 @@ function getRandomHexColor() {
 }
 
 function handlerStop() {
-    refs.startBtn = !isDisabled;
-    refs.stopBtn = isDisabled;
+    toggleBtnState (false, true);
     clearInterval(idColor);
+}
+
+function toggleBtnState(startDisabled, stopDisabled) {
+  refs.startBtn.disabled = startDisabled;
+  refs.stopBtn.disabled = stopDisabled;
 }
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -48,6 +48,11 @@ const counter = {
   const difference = selectedDate - currentDate;
   refs.startBtn.disabled = true;
   updateCounter(convertMs(difference));
+  // const { days, hours, minutes, seconds } = convertMs(difference);
+  // refs.seconds.textContent = seconds;
+  // refs.minutes.textContent = minutes;
+  // refs.hours.textContent = hours;
+  // refs.data.textContent = days;
   if (difference <= 1000) {
     refs.startBtn.disabled = false;
     clearInterval(intervalId);
@@ -72,15 +77,15 @@ function convertMs(ms) {
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-  //updateCounter ({days, hours, minutes, seconds});
-  return { days, hours, minutes, seconds };
+  updateCounter ({days, hours, minutes, seconds});
+  //return { days, hours, minutes, seconds };
 }
 
 function updateCounter ({days, hours, minutes, seconds}) {
-  refs.seconds.textContent = `${seconds}`;
-  refs.minutes.textContent = `${minutes}`;
-  refs.hours.textContent = `${hours}`;
-  refs.data.textContent = `${days}`;
+  refs.seconds.textContent = seconds;
+  refs.minutes.textContent = minutes;
+  refs.hours.textContent = hours;
+  refs.data.textContent = days;
 }
 
 function addLeadingZero(value) {
